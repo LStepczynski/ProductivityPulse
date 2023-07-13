de17eb163f4918016d90a678b8412b6ab0089190
LStepczynski <leonstepczynski@gmail.com>
2023-07-13 04:20:24 -0700

    Gets the focused window, adds it to window list and counts seconds for each element in window list

---
+++ main.py
@@ -0,0 +1,4 @@
+from productivityPulse import ProductivityPulse
+
+if __name__ == '__main__':
+    ProductivityPulse()
---
+++ productivityPulse.py
@@ -0,0 +1,39 @@
+import pygetwindow as gw
+import win32gui, win32process, psutil
+from time import sleep
+
+
+class ProductivityPulse:
+    """Measures how much time is spent on all focused windows"""
+    def __init__(self) -> None:
+        """Checks what window is focused and starts counting"""
+        self.window_list = list()
+        while True:
+            should_add = True # Determines if the focused window should be added to the window list
+            self.focused_window = self.active_window_process_name()
+
+            for element in self.window_list:
+                if element[0] == self.focused_window:
+                    should_add = False
+            
+            if should_add:
+                self.window_list.append([self.focused_window, 0])
+
+            for element in self.window_list:
+                element[-1] += 1
+
+            sleep(1)
+
+    def active_window_process_name(self):
+        try:
+            pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
+            return(psutil.Process(pid[-1]).name())
+        except:
+            pass
+    
+    def active_window_title(self):
+        try:
+            window_handle = win32gui.GetForegroundWindow()
+            window_title = win32gui.GetWindowText(window_handle)
+            return window_title
+        except: pass
