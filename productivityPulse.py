02a8272e8513aa5c9d5dce5fbd8f89bc832fd1f1
LStepczynski <leonstepczynski@gmail.com>
2023-07-13 12:41:09 -0700

    Now user can use a GUI window to check on their time spent on different windows

--- productivityPulse.py
+++ productivityPulse.py
@@ -1,21 +1,17 @@
 import pygetwindow as gw
 import win32gui, win32process, psutil
 from time import sleep
-import keyboard
 from grapths import Plot
 from KandL import extract
 
 
 class ProductivityPulse:
     """Measures how much time is spent on all focused windows"""
-    def __init__(self) -> None:
+    def __init__(self, window_list) -> None:
         """Checks what window is focused and starts counting"""
-        self.window_list = list()
+        self.window_list = window_list
         while True:
             sleep(1)
-
-            if keyboard.is_pressed("ctrl+shift+r"):
-                Plot(extract(self.window_list, 0), extract(self.window_list, -1))
 
             should_add = True # Determines if the focused window should be added to the window list
             self.focused_window = self.active_window_process_name()
@@ -34,16 +30,15 @@ class ProductivityPulse:
                 if element[0] == self.focused_window:
                     element[-1] += 1
 
-            print(self.window_list)
 
-    def active_window_process_name(self):
+    def active_window_process_name(self) -> str:
         try:
             pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
             return(psutil.Process(pid[-1]).name())
         except:
             return None
     
-    def active_window_title(self):
+    def active_window_title(self) -> str:
         try:
             window_handle = win32gui.GetForegroundWindow()
             window_title = win32gui.GetWindowText(window_handle)
