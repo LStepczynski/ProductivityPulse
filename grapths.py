b4d1c2172e4f46fa0ca5cc1ef7cd9dea46e17da0
LStepczynski <leonstepczynski@gmail.com>
2023-08-08 08:03:35 -0700

    Added a way for the user to display the data in a form of a table or a graph

--- grapths.py
+++ grapths.py
@@ -1,14 +1,108 @@
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
 
 class Plot:
-    def __init__(self, names: list, values: list) -> None:
+    def __init__(self, names: list, values: list, mode: str) -> None:
+        self.names = names
+        self.values = [value/60 for value in values]
+        self.mode = mode
+
+        if self.mode == "Graph":
+            self.graph()
+        else: self.table([names, values])
+    
+    def graph(self) -> None:
         # Create the bar graph
-        plt.bar(names, values)
+        plt.bar(self.names, self.values)
 
         # Add labels and title
         plt.xlabel('Windows')
         plt.ylabel('Time')
-        plt.title('Time Spent')
+        plt.title('Time Spent in Minutes')
 
         # Display the plot
         plt.show()
+    
+    def table(self, data: list) -> None:
+        self.names = data[0]
+        self.values = data[1]
+
+        self.root = tk.Tk()
+        self.root.geometry("500x500")
+        self.root.title("Data Display")
+
+        self.data_container = tk.Frame(self.root)
+        self.data_container.pack()
+
+        for index in range(len(self.names)):
+            DataBox(self.data_container, self.names[index], self.values[index], 0, index)
+
+class DataBox():
+    def __init__(self, window, label, value, row, col) -> None:
+        print('p')
+        self.window = window
+        self.label = label
+        self.value = value
+        self.row = row
+        self.col = col
+
+        self.databox = tk.Frame(self.window)
+        self.data_label = tk.Label(self.databox, text=self.label)
+        self.data_value = tk.Label(self.databox, text=self.value)
+        self.databox.grid(row=self.row, column=self.col)
+        self.data_label.pack()
+        self.data_value.pack()
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # style = ttk.Style(self.root)
+        # style.configure("Treeview", rowheight=25, font=('Arial', 12), borderwidth=1)
+        # style.configure("Treeview.Heading", font=('Arial', 14))
+
+        # tree = ttk.Treeview(self.root, columns=data[0], show="headings", style="Treeview")  # Set fixed width here
+
+        # for col in data[0]:
+        #     tree.heading(col, text=col, anchor=tk.CENTER)  # Center-align column headings
+        #     tree.column(col, width=150, anchor=tk.CENTER, stretch=True)  # Center-align column data
+
+        # vsb = ttk.Scrollbar(self.root, orient="horizontal", command=tree.xview)  # Change to xview
+        # tree.configure(xscrollcommand=vsb.set)  # Change to xscrollcommand
+        # vsb.pack(side="bottom", fill="x")  # Change to bottom
+        # tree.pack(fill="both", expand=True)
+
+        # for row in data[1:]:
+        #     tree.insert("", "end", values=row)
+
--- productivity_timer.py
+++ productivity_timer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import datetime
+from winsound import Beep # Replace with your own sound efffect
 
 class Timer:
     """Creates a timer that tells the user when to work and rest"""
@@ -24,19 +25,25 @@ class Timer:
         self.root.mainloop()
     
     def count(self):
-        if not self.root.winfo_exists():
+        if not self.root.winfo_exists(): #Checks if the window exists
             return
-        if self.time == 0:  
+        
+        if self.time == 0:  #When time reaches 0 switches between work and rest
+            Beep(500, 3000)
             if self.state:
                 self.time = self.rest
             else: 
                 self.time = self.work
             self.state = not self.state
-        self.time -= 1
-        if self.state:
+
+        self.time -= 1 #Increment at which the time goes down
+
+        if self.state: #Updates the label that displays time
             self.time_to_change.configure(text=f"Time until rest: {self.convert_seconds(self.time)}")
-        else: self.time_to_change.configure(text=f"Time until work: {self.convert_seconds(self.time)}")
-        self.time_to_change.after(1000, self.count)
+        else: 
+            self.time_to_change.configure(text=f"Time until work: {self.convert_seconds(self.time)}")
+        
+        self.time_to_change.after(1000, self.count) #Runs the function again after 1 second
         
 
     def convert_seconds(self, seconds):
